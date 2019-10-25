a9ddabcd4e001f11088b10d062fc4aa078c2f00f
shibo zhao <505684443@qq.com>
2019-10-25 17:14:31 +0800

    地址列表

--- server/routes/users.js
+++ server/routes/users.js
@@ -180,4 +180,96 @@ router.post("/editCheckAll",(req,res,next)=>{
     }
   })
 })
+//查询用户地址接口
+router.get("/addressList",(req,res,next)=>{
+  var userId = req.cookies.userId
+  User.findOne({userId},(err,doc)=>{
+    if(err){
+      res.json({
+        status:'1',
+        msg:err1.message,
+        result:''
+      })
+    }else{
+      res.json({
+        status:'0',
+        msg:'',
+        result:doc.addressList
+      })
+    }
+  })
+})
+//设置默认地址
+router.post("/setDefault",(req,res,next)=>{
+  var userId = req.cookies.userId,
+      addressId = req.body.addressId
+      if(!addressId){
+        res.json({
+          status:'1003',
+          msg:'addressId is null',
+          result:''
+        })
+      }
+  User.findOne({userId},(err,doc)=>{
+    if(err){
+      res.json({
+        status:'1',
+        msg:err.message,
+        result:''
+      })
+    }else{
+      var addressList = doc.addressList
+      addressList.forEach((item)=>{
+        if(item.addressId == addressId){
+          item.isDefault = true
+        }else{
+          item.isDefault = false
+        }
+      })
+      doc.save((err1,doc1)=>{
+        if(err1){
+          res.json({
+            status:'1',
+            msg:err1.message,
+            result:''
+          })
+        }else{
+          res.json({
+            status:'0',
+            msg:'',
+            result:''
+          })
+        }
+      })
+    }
+  })
+})
+//删除地址
+router.post("/delAddress",(req,res,next)=>{
+  var userId = req.cookies.userId,
+      addressId = req.body.addressId
+      User.update({
+        userId
+      },{
+        $pull:{
+          'addressList':{
+            'addressId':addressId
+          }
+        }
+      },(err,doc)=>{
+        if(err){
+          res.json({
+            status:'1',
+            msg:err.message,
+            result:''
+          })
+        }else{
+          res.json({
+            status:'0',
+            msg:'',
+            result:''
+          })
+        }
+      })
+})
 module.exports = router;
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from './../views/GoodsList'
 import Cart from './../views/Cart'
+import Address from './../views/Address'
 import TestView from './../views/TestView'
 import TestChild from './../views/TestChild'
 import TestChildnew from './../views/TestChildnew'
@@ -21,6 +22,11 @@ export default new Router({
       path: '/cart',
       name: 'Cart',
       component: Cart
+    },
+    {
+      path: '/address',
+      name: 'Address',
+      component: Address
     },
     {
       path: '/test',
